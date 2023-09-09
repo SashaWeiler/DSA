de6cb4b14652e92f2caf8bb90d87381ed2ad9a9e
Sasha Weiler <sasha.p.weiler@gmail.com>
2023-09-09 14:36:34 -0400

    Solution to 26 done

--- Neetcode-Leetcode/Static Arrays/26.py
+++ Neetcode-Leetcode/Static Arrays/26.py
@@ -1,3 +1,16 @@
+arr = [1,1,1,2,2]
 
+def removeDuplicates(arr):
+    x = 1
+    for i in range(1, len(arr)):
+        if arr[i-1] != arr[i]:
+            arr[x] = arr[i]
+            x += 1
+    return x, arr
 
-        
+# removeDuplicates(arr)
+# print(arr)
+
+arr = [0,0,1,1,1,2,2,3,3,4]
+unique, arr = removeDuplicates(arr)
+print(arr)
